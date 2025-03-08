307178857aab821d36b9052ce47821dd8cc6a73e
Praveen Kumar <praveenkumar9894540@gmail.com>
2025-03-08 17:20:16 +0530

    new

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const jwlUserSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ["user", "admin"], default: "user" },
